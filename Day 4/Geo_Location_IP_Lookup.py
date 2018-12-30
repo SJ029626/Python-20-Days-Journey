18d9c96c4040dbec517ad768b1a6d97180b8433e
Sanyam Jain <14850141+SJ029626@users.noreply.github.com>
2018-12-31 02:44:55 +0530

    Locate My IP Address

---
+++ Day 4/Geo_Location_IP_Lookup.py
@@ -0,0 +1,12 @@
+#!/bin/python
+#Geolocation by IpLookup
+from urllib import request
+import json
+
+try:
+    data = json.load(request.urlopen('http://ipinfo.io/json'))
+except Exception as e:
+    print(e)
+else:
+    print('You are near: {city}, {region}, {country}'.format(**data))
+    print('     Lat/Lng: {}E'.format(data['loc'].replace(',','N, ')))
